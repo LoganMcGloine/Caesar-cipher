78127ce866648dbcaed1fc70cd12e1702ae9a4a0
Logan McGloine <136609167+LoganMcGloine@users.noreply.github.com>
2025-02-11 15:08:43 -0500

    Update main.c

--- main.c
+++ main.c
@@ -14,8 +14,8 @@ char *trim(char *s) {
 
 //Encrypts letter using the formula C = E(k,P) = (C +k)(mod 26) then moving it to the correct position in ASCII
 char encryption(char letter) {
-   letter=  toupper(letter);
-    if (letter != 32 ) {letter = ((letter - 'A' + 3) % 26) + 'A'}
+   letter = toupper(letter);
+    if (letter != 32 ) {letter = ((letter - 'A' + 3) % 26) + 'A';}
        printf("%c", letter);
     return letter;
 }
@@ -48,20 +48,6 @@ void decrypt(char word[]) {
     printf(" ");
 }
 
-void encoder(char c) {
-    for (int i = 7; i >= 0; i--) {
-        printf("%c", (c & (1 << i)) ? '1' : '0');
-    }
-    printf(" ");
-}
-
-// Encodes a string into binary
-void encode(char string[]) {
-    int len = strlen(string);
-    for (int i = 0; i < len; i++) {
-        encoder(string[i]);
-    }
-}
 
 int ExitTest;
 int main() {
@@ -80,10 +66,7 @@ int main() {
         else if (strcmp(command, "decrypt") == 0) {
             decrypt(trim(string));
             printf("\n");
-        }else if (strcmp(command, "encode") == 0) {
-            encode(trim(string));
-            printf("\n");
-        }else if (strcmp(command, "exit") == 0) {return 0;}
+        } else if (strcmp(command, "exit") == 0) {return 0;}
         else { printf("please enter command correctly!\n"); }
 
 
